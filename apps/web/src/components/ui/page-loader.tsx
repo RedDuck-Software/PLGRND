4da56ee4dc4fca3ff4fbeab528afc8ff0a2e49b1
MrJeleika <den932005@gmail.com>
2026-05-10 19:29:42 +0200

    feat: replace PLGRND text with logo in PageLoader component

--- apps/web/src/components/ui/page-loader.tsx
+++ apps/web/src/components/ui/page-loader.tsx
@@ -2,7 +2,7 @@ export const PageLoader = () => {
   return (
     <div className="w-screen h-screen bg-background absolute flex items-center justify-center inset-0 z-50">
       <div className="flex items-center gap-2">
-        <h1 className="font-mono sm:text-6xl text-xl">PLGRND</h1>
+        <img src="/logo-light.png" alt="logo" className="sm:w-[200px] sm:h-[200px] w-10 h-10" />
       </div>
     </div>
   )
