cf6f248bc434ad297e85c26a8b64a9571226f9c1
MrJeleika <den932005@gmail.com>
2026-05-10 10:55:22 +0200

    feat: rebrand PageLoader to PLGRND

--- apps/web/src/components/ui/page-loader.tsx
+++ apps/web/src/components/ui/page-loader.tsx
@@ -3,7 +3,7 @@ export const PageLoader = () => {
     <div className="w-screen h-screen bg-background absolute flex items-center justify-center inset-0 z-50">
       <div className="flex items-center gap-2">
         <img src="/logo-light.svg" alt="logo" className="sm:w-24 sm:h-24 w-10 h-10" />
-        <h1 className="font-mono sm:text-6xl text-xl">SOL LEARN</h1>
+        <h1 className="font-mono sm:text-6xl text-xl">PLGRND</h1>
       </div>
     </div>
   )
